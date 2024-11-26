e7c879b22e6abd9af47467b5ae315b4fa4137ac0
david <david@burkesoftware.com>
2024-11-26 09:58:51 -0500

    Fix marketing site

--- projects/marketing/src/app/documentation/documentation-page.component.ts
+++ projects/marketing/src/app/documentation/documentation-page.component.ts
@@ -4,32 +4,32 @@ import { ActivatedRoute, RouterLink } from "@angular/router";
 import { MarkdownComponent, MarkdownService } from "ngx-markdown";
 
 @Component({
-    imports: [MatCard, MatCardContent, RouterLink, MarkdownComponent],
-    templateUrl: "./documentation-page.component.html",
-    styleUrls: ["./documentation-page.component.scss"]
+  imports: [MatCard, MatCardContent, RouterLink, MarkdownComponent],
+  templateUrl: "./documentation-page.component.html",
+  styleUrls: ["./documentation-page.component.scss"]
 })
 export class DocumentationPageComponent implements OnInit {
   slug: string | null = null;
   constructor(
     private route: ActivatedRoute,
     private markdownService: MarkdownService,
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     const locationPrefix = `/documentation/${this.route.snapshot.params.slug}`;
 
-    this.markdownService.renderer.heading = (text: string, level: number) => {
+    this.markdownService.renderer.heading = ({ text, depth }) => {
       const escapedText = text.toLowerCase()
-      // replace non-letter characters with hyphens
-      .replace(/[^\w]+/g, "-")
-      //trim hyphens at end of string
-      .replace(/\-$/, '');
+        // replace non-letter characters with hyphens
+        .replace(/[^\w]+/g, "-")
+        //trim hyphens at end of string
+        .replace(/\-$/, '');
       return (
-        `<h${level}>` +
+        `<h${depth}>` +
         `<a id="${escapedText}" class="anchor" href="${locationPrefix}#${escapedText}">` +
         text +
         "</a>" +
-        `</h${level}>`
+        `</h${depth}>`
       );
     };
 
