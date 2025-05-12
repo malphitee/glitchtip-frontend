1ff9d98a964c768c1a4984be31d944830b02fd86
James Kiger <james.kiger@gmail.com>
2025-05-12 14:20:24 -0400

    Use ConfirmDialog instead of window.confirm

--- src/app/profile/auth-tokens/auth-tokens.component.ts
+++ src/app/profile/auth-tokens/auth-tokens.component.ts
@@ -2,11 +2,13 @@ import { Component, computed, OnInit, inject } from "@angular/core";
 import { AuthTokensService, AuthTokensState } from "./auth-tokens.service";
 import { LoadingButtonComponent } from "../../shared/loading-button/loading-button.component";
 import { CopyInputComponent } from "../../shared/copy-input/copy-input.component";
+import { MatDialog } from "@angular/material/dialog";
 import { MatDividerModule } from "@angular/material/divider";
 import { RouterLink } from "@angular/router";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { StatefulComponent } from "src/app/shared/stateful-service/signal-state.component";
+import { ConfirmDialogComponent } from "src/app/shared/confirm-dialog/confirm-dialog.component";
 
 @Component({
   selector: "gt-auth-tokens",
@@ -26,6 +28,7 @@ export class AuthTokensComponent
   implements OnInit
 {
   protected service: AuthTokensService;
+  dialog = inject(MatDialog);
 
   authTokens = computed(() =>
     this.service.apiTokens()?.map((token) => {
@@ -49,11 +52,21 @@ export class AuthTokensComponent
   }
 
   deleteAuthToken(id: number) {
-    if (
-      window.confirm(
-        "Are you sure you want to delete this authentication token?",
-      )
-    )
-      this.service.deleteAuthToken(id);
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      restoreFocus: false,
+      height: "200px",
+      width: "350px",
+      data: {
+        title: $localize`Delete Auth Token`,
+        message: $localize`Are you sure you want to delete this authentication token?`,
+        confirmText: $localize`Delete`,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((confirmed) => {
+      if (confirmed) {
+        this.service.deleteAuthToken(id);
+      }
+    });
   }
 }
