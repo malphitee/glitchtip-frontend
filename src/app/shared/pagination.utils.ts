62c8b40a47ae74db62244f79aa1cb5db30198383
David Burke <david@burkesoftware.com>
2025-05-21 15:45:29 -0400

    Load in organization pages

--- src/app/shared/pagination.utils.ts
+++ src/app/shared/pagination.utils.ts
@@ -60,6 +60,43 @@ export function getPaginationHeaders(response: {
   };
 }
 
+export function getCursor(response: { headers: Headers }) {
+  const nextPageURL = getPaginationHeaders(response).nextPageURL;
+  if (nextPageURL) {
+    return new URLSearchParams(new URL(nextPageURL).search).get("cursor");
+  }
+  return null;
+}
+
+/**
+ * Updates a target array with items from a source array based on their 'id' property.
+ * If an item with the same 'id' in the source array exists in the target array, it's updated.
+ * Otherwise, the item from the source array is added to the target array.
+ *
+ * @template T An object type that must include an 'id' property (string or number).
+ * @param {T[]} targetArray The array to be updated.
+ * @param {T[]} sourceArray The array containing items to update or add.
+ * @returns {T[]} The updated target array.
+ */
+export function updateArrayById<T extends { id: string | number }>(
+  targetArray: T[],
+  sourceArray: T[],
+): T[] {
+  sourceArray.forEach((sourceItem) => {
+    const index = targetArray.findIndex(
+      (targetItem) => targetItem.id === sourceItem.id,
+    );
+    if (index >= 0) {
+      // Item found, update it
+      targetArray[index] = sourceItem;
+    } else {
+      // Item not found, add it
+      targetArray.push(sourceItem);
+    }
+  });
+  return targetArray;
+}
+
 /** Compute paginator object which contains convenience fields such as human readable count */
 export function getPaginator(pagination: PaginationState | undefined) {
   if (pagination === undefined) {
