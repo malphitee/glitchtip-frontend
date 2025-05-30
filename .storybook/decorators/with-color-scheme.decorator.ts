4a1a95a20dd0981a5af9797e9c1db86748c53cca
David Burke <david@burkesoftware.com>
2025-05-30 12:00:13 -0400

    npm i works again

--- .storybook/decorators/with-color-scheme.decorator.ts
+++ .storybook/decorators/with-color-scheme.decorator.ts
@@ -1,28 +1,17 @@
 import { Decorator } from "@storybook/angular";
-import { useDarkMode } from "storybook-dark-mode";
 
 export const withColorScheme: Decorator = (story, context) => {
-  const isDarkMode = useDarkMode();
-
   const document = window.parent.document;
   const iframe = document.getElementById(
-    "storybook-preview-iframe"
+    "storybook-preview-iframe",
   ) as HTMLIFrameElement;
   const iframeDocument =
     iframe?.contentDocument || iframe?.contentWindow?.document;
 
   if (iframeDocument) {
-    const root = iframeDocument.documentElement;
+    // const root = iframeDocument.documentElement;
     // Add mat 3 dark mode
-    root.setAttribute("style", isDarkMode ? "color-scheme: dark;" : "");
-    // mat 2, remove when upgrading to mat 3
-    if (isDarkMode) {
-      root.classList.remove("light");
-      root.classList.add("dark");
-    } else {
-      root.classList.remove("dark");
-      root.classList.add("light");
-    }
+    // root.setAttribute("style", isDarkMode ? "color-scheme: dark;" : "");
   }
 
   return story();
