83ede60f3ced3b8352f12e29260487477d9ad4a8
David Burke <david@burkesoftware.com>
2025-06-05 10:08:51 -0400

    Refactor member detail

--- src/app/api/organizations/members-api.service.ts
+++ src/app/api/organizations/members-api.service.ts
@@ -1,11 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
 import { baseUrl } from "../../constants";
-import {
-  Member,
-  MemberDetail,
-  OrgMemberUpdate,
-} from "./organizations.interface";
+import { Member, MemberDetail } from "./organizations.interface";
 
 @Injectable({
   providedIn: "root",
@@ -23,19 +19,12 @@ export class MembersAPIService {
     return this.http.get<MemberDetail>(this.detailURL(orgSlug, memberId));
   }
 
-  update(orgSlug: string, memberId: number, memberUpdateData: OrgMemberUpdate) {
-    return this.http.put<Member>(
-      this.detailURL(orgSlug, memberId),
-      memberUpdateData,
-    );
-  }
-
-  makeOrgOwner(orgSlug: string, memberId: number) {
-    return this.http.post<Member>(
-      this.detailURL(orgSlug, memberId) + "set_owner/",
-      null,
-    );
-  }
+  // update(orgSlug: string, memberId: number, memberUpdateData: OrgMemberUpdate) {
+  //   return this.http.put<Member>(
+  //     this.detailURL(orgSlug, memberId),
+  //     memberUpdateData,
+  //   );
+  // }
 
   private listURL(organizationSlug: string) {
     return `${baseUrl}/organizations/${organizationSlug}${this.url}`;
