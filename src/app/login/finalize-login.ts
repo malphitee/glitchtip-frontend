d85beaf6c998ec07c7e282679cd8d1b7ab22a8fb
pete davids <pete.davids@layerhealth.com>
2025-10-23 09:06:18 -0400

    propagate next param through social login

--- src/app/login/finalize-login.ts
+++ src/app/login/finalize-login.ts
@@ -6,6 +6,7 @@ import {
 } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthService } from "../auth.service";
+import { LoginService } from "./login.service";
 
 // This component is used for waiting until the auth status
 // of a redirected social auth user is confirmed before
@@ -17,12 +18,13 @@ import { AuthService } from "../auth.service";
 export class FinalizeLogin {
   private authService = inject(AuthService);
   private router = inject(Router);
+  private loginService = inject(LoginService);
 
   constructor() {
     effect(() => {
       if (this.authService.initialized()) {
         if (this.authService.isAuthenticated()) {
-          this.router.navigate(["/"]);
+          this.loginService.redirect();
         } else {
           this.router.navigate(["/login"]);
         }
